1a5dcbedb1f598e4452641ee171a9b5ee6d438bf
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-04-01 06:35:00 +0300

    Complete Assigment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const data = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(props.setPage("List"));
   }
 
   return (
@@ -29,8 +46,8 @@ function QuestionForm(props) {
         <label>
           Prompt:
           <input
-            type="text"
-            name="prompt"
+            type='text'
+            name='prompt'
             value={formData.prompt}
             onChange={handleChange}
           />
@@ -38,8 +55,8 @@ function QuestionForm(props) {
         <label>
           Answer 1:
           <input
-            type="text"
-            name="answer1"
+            type='text'
+            name='answer1'
             value={formData.answer1}
             onChange={handleChange}
           />
@@ -47,8 +64,8 @@ function QuestionForm(props) {
         <label>
           Answer 2:
           <input
-            type="text"
-            name="answer2"
+            type='text'
+            name='answer2'
             value={formData.answer2}
             onChange={handleChange}
           />
@@ -56,8 +73,8 @@ function QuestionForm(props) {
         <label>
           Answer 3:
           <input
-            type="text"
-            name="answer3"
+            type='text'
+            name='answer3'
             value={formData.answer3}
             onChange={handleChange}
           />
@@ -65,8 +82,8 @@ function QuestionForm(props) {
         <label>
           Answer 4:
           <input
-            type="text"
-            name="answer4"
+            type='text'
+            name='answer4'
             value={formData.answer4}
             onChange={handleChange}
           />
@@ -74,17 +91,17 @@ function QuestionForm(props) {
         <label>
           Correct Answer:
           <select
-            name="correctIndex"
+            name='correctIndex'
             value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
+            onChange={handleChange}>
+            <option value='0'>{formData.answer1}</option>
+            <option value='1'>{formData.answer2}</option>
+            <option value='2'>{formData.answer3}</option>
+            <option value='3'>{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+
+        <button type='submit'>Add Question</button>
       </form>
     </section>
   );
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestion] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => setQuestion(data));
+  }, []);
+  function onDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() =>
+        setQuestion((questions) => questions.filter((q) => q.id !== id))
+      );
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
